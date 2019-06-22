9408bfb5e5bf6f80076de53d6a0e65e5e0740e0c
AnnWhite007 <anuta_myrzilka@inbox.ru>
2019-06-22 20:10:47 +0300

    1.4. JUnit[#147723]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,6 +4,8 @@ package ru.job4j.calculate;
  * Calculate.
  *
  * @author Anna Babintseva (anuta_myrzilka@inbox.ru)
+ * @version $Id$
+ * @since 0.1
  */
 public class Calculate {
 	/**
@@ -13,4 +15,13 @@ public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+	
+	/**
+	 * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
