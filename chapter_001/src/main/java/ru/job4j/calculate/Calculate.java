bf2f692474bb9f38a91ac3ca8f99d802b90e95fe
AnnWhite007 <anuta_myrzilka@inbox.ru>
2019-06-22 18:55:30 +0300

    1.3. JavaDoc. Code convention[#147722]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
-package main.java.ru.job4j.calculate;
+package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ * @author Anna Babintseva (anuta_myrzilka@inbox.ru)
+ */
 public class Calculate {
-	
+	/**
+	* Main
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Anna Babintseva (anuta_myrzilka@inbox.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
