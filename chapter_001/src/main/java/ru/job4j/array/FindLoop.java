d5d527c3f5e95f55d271cee126935e77f894850c
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-01-05 21:03:12 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,7 +1,18 @@
 package ru.job4j.array;
 
+/**
+ * Class FindLoop
+ * 6.1. Классический поиск перебором.
+ * У нас задан массив элементов. Нам необходимо найти индекс элемента, удовлетворяющего условию.
+ * Решение этой задачи сводится в последовательному перебору каждого элемента.
+ * Если элемент подходит под условие, мы останавливаем цикл и возвращаем индекс нашего элемента.
+ * 6.5. Поиск индекса в диапазоне.
+ * Нужно усовершенствовать классический поиск.
+ * Теперь нужно сделать поиск по массиву, но не во всем массиве, а только в указанном диапазоне.
+ */
+
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = 0; index < data.length; index++ ) {
             if (data[index] == el) {
@@ -10,5 +21,17 @@ public class FindLoop {
             }
         }
         return rst;
+    }
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        // data - массив чисел, el - элемент, который нужно найти,
+        // start - индекс с которого начинаем поиск, finish - индекс которым заканчиваем поиск
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el){
+                rst = index;
+                break;
+            }
+        }
+            return rst;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
-
+    //для первого метода
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
@@ -31,6 +31,27 @@ public class FindLoopTest {
         int value = 13;
         int result = find.indexOf(input, value);
         int expect = 3;
+        assertThat(result, is(expect));
+    }
+   // для второго метода
+    @Test
+    public void whenFind3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind2() {
+        int[] data = new int[] {6, 11, 9, 1, 0, 9, 4};
+        int el = 9;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expect = 2;
         assertThat(result, is(expect));
     }
 }
