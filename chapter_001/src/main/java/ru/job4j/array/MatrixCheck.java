9955b040517bfe15f860175565ab6253cc589424
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-01-06 13:46:52 +0300

    6.7.1. Моно строка в матрице. [#214126]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,73 +1,20 @@
 package ru.job4j.array;
 
+/**
+ * Class MatrixCheck
+ * 6.7.1. Моно строка в матрице.
+ * Проверить, что строка в двухмерном массива целиком заполнена символом 'X'
+ */
+
 public class MatrixCheck {
-    public static boolean isWin(char[][] board) {
-        boolean result = false;
-        for (int row = 0; row < board.length; row++) {
-            for (int cell = 0; cell < board.length; cell++) {
-                char sign = board[row][cell];
-                System.out.print(sign);
-                //for () { проверить последовательность.
-                for (cell = 0; cell < board.length; cell++) {
-                    if (board[0][cell] == 'X') {
-                        for (row = 0; row < board.length; row++) {
-                            if (board[row][cell] == 'X') {
-                                result = true;
-                            } else {
-                                result = false;
-                                break;
-                            }
-                        }
-                    } else {
-                        for (row = 0; row < board.length; row++) {
-                            if (board[row][0] == 'X') {
-                                for (cell = 0; cell < board.length; cell++) {
-                                    if (board[row][cell] == 'X') {
-                                        result = true;
-                                    } else {
-                                        result = false;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[row][index] != 'X') {
+                result = false;
+                break;
             }
-            System.out.println();
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        char[][] hasWinVertical = {
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-        };
-        boolean win = isWin(hasWinVertical);
-        System.out.println("A board has a winner : " + win);
-        System.out.println();
-        char[][] hasWinHor = {
-                {'_', '_', '_', '_', '_'},
-                {'X', 'X', 'X', 'X', 'X'},
-                {'_', '_', '_', '_', '_'},
-                {'_', '_', '_', '_', '_'},
-                {'_', '_', '_', '_', '_'},
-        };
-        boolean winHor = isWin(hasWinHor);
-        System.out.println("A board has a winner : " + winHor);
-        System.out.println();
-        char[][] notWin = {
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', 'X', '_', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-        };
-        boolean lose = isWin(notWin);
-        System.out.println("A board has a winner : " + lose);
     }
 }
