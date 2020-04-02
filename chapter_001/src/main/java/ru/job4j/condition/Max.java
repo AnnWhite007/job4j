467f06d9f940ee8547b9ad662db6b86b251e0836
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-04-02 22:52:28 +0300

    3. Перегрузить метод max для трех чисел.

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -3,6 +3,10 @@ package ru.job4j.condition;
 /**
  * 4.2. Максимум из двух чисел.
  * Сравнить два числа и получить вернуть максимальное из них.
+ *
+ * 3. Перегрузить метод max для трех чисел.
+ * В Java есть механизм позволяющий создавать методы со одинаковыми именами,
+ * но с разными входными параметрами.
  */
 
 public class Max {
@@ -10,4 +14,9 @@ public class Max {
         int result = left > right ? left : right;
         return result;
     }
+    public static int max(int left, int right, int front) {
+        int result = max(left, right) > front ? max(left, right) : front;
+        return result;
+    }
+
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
     public class MaxTest {
+        /*
         @Test
         public void whenMax1To2Then2() {
             int result = Max.max(1, 2);
@@ -16,6 +17,21 @@ import static org.junit.Assert.assertThat;
         }
         public void whenMax2To2Then2() {
             int result = Max.max(2, 2);
+            assertThat(result, is(2));
+        }
+
+         */
+        @Test
+        public void whenMax1To2Then2() {
+            int result = Max.max(1, 2, 3);
+            assertThat(result, is(3));
+        }
+        public void whenMax2To1Then1() {
+            int result = Max.max(4, 1, 2);
+            assertThat(result, is(4));
+        }
+        public void whenMax2To2Then2() {
+            int result = Max.max(2, 2, 2);
             assertThat(result, is(2));
         }
     }
