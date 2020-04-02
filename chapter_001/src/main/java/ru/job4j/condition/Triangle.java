323872f1e12c04d3f76b9b513121cdaf872eed5a
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-04-02 17:52:48 +0300

    2. Рефакторинг - Площадь треугольника.

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -6,12 +6,89 @@ package ru.job4j.condition;
  * треугольник может существовать, если сумму двух сторон больше третьей для всех комбинаций.
  * Допустим у нас заданы три переменные
  * double ab, ac, dc - это длины сторон.
+ *
+ * 2. Рефакторинг - Площадь треугольника.
+ * Создадим три поля с типом Point.
+ * Создадим конструктор с тремя переменными типа Point.
+ * Проинициализируем начальная состояние объекта Triangle c помощью входящих параметров.
+ * Поправим метод area. Нужно убрать входящие параметры, вместо них нужно использовать поля объекта
  */
-
+/*
 public class Triangle {
 
     public static boolean exist(double ab, double ac, double bc) {
         if (ab + ac > bc && ac + bc > ab && ab + bc > ac) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+ */
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     * Метод вычисления  полу-периметра по длинам сторон.
+     *
+     * Формула.
+     *
+     * (a + b + c) / 2
+     *
+     * @param a расстояние между точками a b
+     * @param b расстояние между точками a c
+     * @param c расстояние между точками b c
+     * @return полу-Периметр.
+     */
+    public double period(double a, double b, double c) {
+        return ((a + b + c) / 2);
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * Формула.
+     *
+     * √ p *(p - ab) * (p - ac) * (p - bc)
+     *
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            // написать формулу для расчета площади треугольника.
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param a Длина от точки a b.
+     * @param b Длина от точки a c.
+     * @param c Длина от точки b c.
+     * @return
+     */
+    private boolean exist(double a, double c, double b) {
+        if (a + b > c && b + c > a && a + c > b) {
             return true;
         } else {
             return false;
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertThat;
 
 @Ignore
 public class TriangleTest {
+
+    /*
     @Test
     public void whenExist() {
         boolean result = Triangle.exist(2.0, 2.0, 2.0);
@@ -16,4 +18,24 @@ public class TriangleTest {
         boolean result = Triangle.exist(2.0, 2.0, 1.0);
         assertThat(result, is(false));
     }
+
+     */
+    @Test
+    public void whenExist() {
+        Point first = new Point(6, 1);
+        Point second = new Point(2, 1);
+        Point third = new Point(3, 3);
+        Triangle check = new Triangle(first, second, third);
+        double result = check.area();
+        assertThat(result, is(4.000000000000001));
+    }
+    public void whenNotExist() {
+        Point first = new Point(1, 1);
+        Point second = new Point(2, 2);
+        Point third = new Point(3, 3);
+        Triangle check = new Triangle(first, second, third);
+        double result = check.area();
+        assertThat(result, is(-1));
+    }
+
 }
