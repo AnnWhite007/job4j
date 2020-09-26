3a16d5ff71d523efacb6a8d51946bee7d9be9012
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-09-26 18:56:31 +0300

    1.1.1. Конструктор по умолчанию [#371415]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+/**
+ * 2.1.1.1. Конструктор по умолчанию
+ * В методе main мы вызываем конструктор, хотя описания этого конструктора в классе нет.
+ */
+public class Fix {
+    public static void main(String[] args) {
+        Fix item = new Fix();
+    }
+}
