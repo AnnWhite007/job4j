4b4b7484dd066c27cb6b149b2c9621a5ad382567
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-09-26 21:48:42 +0300

    1.3. Вызов метода с аргументами. [#371411]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+/**
+ * 2.1.3. Вызов метода с аргументами.
+ *в методах можно указать входящие параметры.
+ * Термин "входящие параметры" обозначает переменные,
+ * которые будут доступны внутри метода
+ * и которые нужно проинициализировать при вызове этого метода.
+ */
+public class Jukebox {
+    public static void main(String[] args) {
+        Jukebox one = new Jukebox();
+        one.music(1);
+    }
+    public void music(int position) {
+        if (position == 1) {
+        System.out.println("Пусть бегут неуклюже");
+    } else if (position == 2) {
+            System.out.println("Пусть бегут неуклюже");
+        }
+        else {
+            System.out.println("Песня не найдена");
+        }
+    }
+}
