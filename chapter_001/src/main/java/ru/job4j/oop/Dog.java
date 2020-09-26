399c4c3e770f5516598aeb7e18b7fe1be0ac7be5
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-09-26 18:50:16 +0300

    1.1. Создание объекта [#371408]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+/**
+ * 1.2.1.1. Создание объекта
+ */
+public class Dog {
+    public static void main(String[] args) {
+        //создаем переменную типа Dog с именем polkan...
+        //Оператор new резервирует кусок памяти и записывает в нее данные объекта класса Dog.
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
