31b419cd0a34cd19bca85fcc4c1b753dcedeafe9
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-10-17 17:28:07 +0300

    6. Почему наследование - это плохо [#371424]

---
+++ chapter_001/src/main/java/ru/job4j/oop/LiquidationProduct.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+/**
+ * 2.6. Почему наследование - это плохо
+ * Чтобы запретить наследование нужно использовать ключевое слово final.
+ */
+
+public final class LiquidationProduct {
+
+
+   /*
+   private Product product;
+
+    public LiquidationProduct(String name, int price) {
+        product = new Product(name, price);
+    }
+
+    public String label() {
+        return product.label();
+    }
+   */
+}
