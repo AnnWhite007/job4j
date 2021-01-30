0392a8ad9cdbbeced2b1d9f42f5c9023f8655715
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-01-31 02:07:44 +0300

    3.1. Массив с пустыми ячейками [#371432]

---
+++ chapter_001/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,23 @@
+package ru.job4j.pojo;
+
+/**
+ * 3.1. Массив с пустыми ячейками
+ * Если мы попробуем получить данные из пустых ячеек, то мы получим null-ссылку. Это значение не существующего объекта.
+ * У значения null нельзя вызывать методы объекта. Это приведет к ошибке NullPointerException.
+ * Поэтому, если в коде есть ситуация, что переменная может содержать значение null,
+ * а нам нужно с ней работать, то нужно перед выполнением операции проверить, что объект не равен null.
+ */
+
+public class Shop {
+
+    public static int indexOfNull(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+           // Product product = products[i];
+            if (products[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
