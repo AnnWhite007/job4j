b72846acac75693fbdfb31898fc1453fbd4c2ebe
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-02-24 13:52:12 +0300

    Коррекция Item для 8. Реализация меню за счет шаблона стратегия. [#371365]

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -5,14 +5,22 @@ import java.util.Objects;
  * Класс Item описывает модель заявления.
  * Поле id - это уникальный номер заявления.
  * Поле name содержит название заявления.
+ *
+ *  2.7. toString
+ *  *     Метод toString() служит, чтобы код можно было переиспользовать и не думать о том,
+ *  *     как выводить строковое представление объектов,
+ *  *     определен в классе  Object, наследниками которого по умолчанию являются все классы в Java.
+ *  *     Для того чтобы этот метод использовать в своих классах,
+ *  *     необходимо использовать механизм переопределения.
 */
 public class Item {
-    public Item(String name) {
-        this.name = name;
-    }
 
     private int id;
     private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
 
     public int getId() {
         return id;
@@ -28,5 +36,10 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + ", name: " + name;
     }
 }
