f494c315728baa638c9a66d5c9d241db656f5441
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-03-17 16:18:34 +0300

    4. final [#371397]

---
+++ chapter_002/src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,57 @@
+package ru.job4j.ooa;
+
+/**
+ * Модификатор final - "окончательный, последний". Ключевое слово чтобы сделать дальнейшее изменение объекта невозможным.
+ * если мы задали переменную у которой есть ключевое слово final, то изменить ее мы не сможем.
+ * final для методов - при наследовании данный метод нельзя будет переопределить
+ * Final для классов - не сможем создать наследников этого класса, в какой-то степени, и все его методы тоже становятся final
+ * Важная особенность поведения ключевого слова final для ссылочных типов данных - после присвоения объекта, нельзя изменить ссылку на данный объект.
+ * Однако при этом мы можем изменить внутреннее состояние объекта.
+ * Необходимо чтобы класс Airbus нельзя было наследовать;
+ * Метод printCountEngine() должен выводить реальное количество двигателей,
+ * которое присуще той или иной модели самолета.
+ * Поэтому если модель самолета A380 - количество двигателей должно равняться 4,
+ * для всех остальных моделей - используем в качестве значения константу COUNT_ENGINE.
+ */
+
+public final class Airbus extends Aircraft {
+    // константа на уровне класса
+    private static int COUNT_ENGINE = 2;
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static int getCountEngine() {
+        return COUNT_ENGINE;
+    }
+
+    public static void setCountEngine(int countEngine) {
+        COUNT_ENGINE = countEngine;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
