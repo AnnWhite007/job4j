2daa8b97d7d9ab20f07bcbf27bee48aa7318c5f8
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-04-07 21:36:57 +0300

    0. Коллекции, ArrayList, List, Обобщения [#371342]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+/**
+ * 0. Коллекции, ArrayList, List, Обобщения
+ * Коллекции - это классы, позволяющие работать с наборами однотипных данных.
+ * В Java основная структура для работы с наборами данных - это массив.
+ * Все классы коллекций внутри используют либо массив, либо связанные списки.
+ * Всего есть 4 базовых операции.
+ *   - вставка данных names.add("");
+ *   - замена данных names.set(0, "Ivan");
+ *   - удаление данных names.remove(0);;
+ *   - чтение данных через for-each
+ *   Элемент языка - обобщение -  механизм позволяет в процессе компиляции проверить,
+ *   что в коллекцию добавляют элементы только указанного типа.
+ */
+
+public class UsageArrayList {
+    public static void main(String[] arg) {
+        ArrayList<String> names = new ArrayList<String>();
+        //Добавление данных.
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        //Чтение данных.
+        for (Object value : names) {
+            System.out.println(value);
+        }
+    }
+}
