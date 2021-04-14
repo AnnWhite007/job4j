79733eab5503f8e8b71b3ea5743e322cdd998c05
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-04-14 21:14:31 +0300

    2. Идентичные тексты [#371350]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+/**
+ * 2. Идентичные тексты
+ * Нам нужно написать метод, который будет сравнивать два текста.
+ * Если они состоят из один и тех же слов, то мы считаем, что текст не уникальный.
+ * Метод contains - принимает элемент коллекции и возвращает true, если элемент уже есть или false, если его нет.
+ */
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        // Преобразуем строку в массив слов.
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        // Массив строк вставить через цикл for-each в коллекцию HashSet
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String wordCheck : text) {
+            if (!check.contains(wordCheck)){
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
