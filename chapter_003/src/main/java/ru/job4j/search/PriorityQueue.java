257f4bc9cf0640a796f8fa0eaef2af57f196c694
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-04-14 12:43:55 +0300

    2. Очередь с приоритетом на LinkedList [#371337]

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,36 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+/**
+ * 2. Очередь с приоритетом на LinkedList - этот класс внутри использует связанные списки.
+ * В Java коллекции представляют собой фреймворк/набор классов, которые можно легко расширить.
+ * Если ячейка уже занята, то элементы сдвигаются вправо, а не заменяются, как в массиве.
+ *
+ * Очередь с приоритетами - это коллекция,
+ * в которой элементы при добавлении располагаются в определенном порядке.
+ * Метод должен вставлять в нужную позицию элемент.
+ * Позиция определяется по полю приоритет.
+ * Для вставки использовать add(int index, E value)
+ *
+ */
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    // В методе put вам нужно найти index нужного элемента
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (element.getPriority() < task.getPriority()) {
+                index++;
+            }
+        }
+            this.tasks.add(index, task);
+    }
+
+    //Метод take возвращает первый элемент из списка и удаляет его
+    public Task take() {
+        return tasks.poll();
+    }
+}
