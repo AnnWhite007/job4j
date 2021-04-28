c834034704006955fc85d6e95925138ccf06f51f
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-04-29 00:57:25 +0300

    2. Функциональный интерфейс [#371162] v.02

--- chapter_004/src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ chapter_004/src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -70,7 +70,7 @@ public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
         // вставка элементов в карту с использованием BiConsumer
-        BiConsumer<Integer, String> biCon = (s, s1) -> map.put(s, s1);
+        BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
         biCon.accept(1, "one");
         biCon.accept(2, "two");
         biCon.accept(3, "three");
@@ -79,18 +79,18 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (s, s1) -> s % 2 == 0 || s1.length() == 4;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
-                System.out.println("key: " + biPred.test(i, map.get(i)) + " value: " + map.get(i));
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
             }
-
-        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
-        Consumer<String> con = (s) -> System.out.println(s);
-        Function<String, String> func = s -> s.toUpperCase();
-        for (String s : sup.get()) {
-            con.accept(s);
-            func.apply(s);
-            System.out.println(func.apply(s));
         }
+            Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+            List<String> strings = sup.get();
+            Consumer<String> con = (s) -> System.out.println(s);
+            Function<String, String> func = s -> s.toUpperCase();
+            for (String s : strings) {
+                con.accept(func.apply(s));
+            }
     }
 }
