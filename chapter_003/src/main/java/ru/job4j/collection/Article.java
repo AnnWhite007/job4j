b10c57861921c263c9ee7313923438e7ced2c102
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-05-06 11:18:24 +0300

    1. Генератор текста v.02

--- chapter_003/src/main/java/ru/job4j/collection/Article.java
+++ chapter_003/src/main/java/ru/job4j/collection/Article.java
@@ -11,16 +11,10 @@ import java.util.HashSet;
 public class Article {
     public static boolean generateBy(String origin, String line) {
 
-        String[] originArray = origin.split(" ");
-        String[] lineArray = line.split(" ");
+        String[] originArray = origin.replaceAll("\\p{P}", "").split(" ");
+        String[] lineArray = line.replaceAll("\\p{P}", "").split(" ");
         HashSet<String> originSet = new HashSet<String>();
         for (String value : originArray) {
-            if (value.contains(",")
-                    || value.contains(":")
-                    || value.contains(".")
-                    || value.contains("!")) {
-                originSet.add(value.substring(0, value.length() - 1));
-            }
             originSet.add(value);
         }
         for (String value : lineArray) {
