87ab6c7ba6041a09574fbb2aac3fe4c9a1824568
AnnWhite007 <anuta_myrzilka@inbox.ru>
2021-05-07 13:19:38 +0300

    0. Stream API улучшения [#371181]

---
+++ chapter_004/src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,22 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 0. Stream API улучшения
+ * Stream.takeWhile - этот метод позволяет получать поток данных до тех пор, пока условие истина.
+ * Stream.dropWhile - этот метод позволяет получать поток данных после того, как условие стало ложным.
+ * Stream.ofNullable - сделает из элемента поток, если элемент равен null, то элемент пропускается.
+ */
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .sorted((left, right) -> right.getScore() - left.getScore())
+                .takeWhile(st -> st.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
