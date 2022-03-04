3c3180691c42143fc5eb36eb43dff637f41cddd9
AnnWhite007 <anuta_myrzilka@inbox.ru>
2022-03-04 13:45:03 +0300

    Коррекция проекта по checkstyle (дополнение)

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -21,7 +21,6 @@ public class Board {
     public static void paint(int width, int height) {
         for (int i = 1; i <= width; i++) {
             for (int j = 1; j <= height; j++) {
-                //
                 if ((i + j) % 2 == 0) {
                     System.out.print("X");
                 } else {
